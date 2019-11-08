1a6fa4c018f64a41b3a489b2f7d2febd5318e96c
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-11-08 10:43:12 -0600

    still trying to get heroku to work

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,6 @@ router.use("/api", apiRoutes);
 router.use('/user', user)
 
 // If no API routes are hit, send the React app
-router.use(function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+
 
 module.exports = router;
