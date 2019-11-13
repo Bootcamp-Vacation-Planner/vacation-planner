f281ac774a2b7b672218a6ad76a19c82b0cf2da8
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-11-13 11:54:35 -0600

    moved the axios post call from home.js to API.js in the utils folder

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export default {
     eventPost: function (data) {
-    return axios.post('/api/events', {
-        data
-      })
+    return axios.post('/api/events', 
+        data)
     }
 }
